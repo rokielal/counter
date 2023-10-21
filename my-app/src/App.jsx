7aa53dbe18108d0f9759019a46d039e777268749
Manoranjan <manoranjangupta94@gmail.com>
2023-10-21 19:30:40 +0530

    Removing value

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   const [count, setCount] = useState(0);
 
-  const handleIncrease = (value) => {
+  const handleIncrease = () => {
     return(
       setCount(count + 1)
       
